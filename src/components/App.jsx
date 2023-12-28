d7941b29dcd8e7683766f27ebb232deb1f0e8d32
TokarevArtemV <tokarev.artem.v@gmail.com>
2023-12-29 01:24:56 +0200

    change to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
@@ -9,114 +9,88 @@ import {
 import 'react-notifications/lib/notifications.css';
 import { Modal } from './Modal';
 
-const KEY_LOCAL_STORADGE = 'contacts';
+const KEY_LOCAL_STORAGE = 'contacts';
 
-export class App extends PureComponent {
-  state = {
-    contacts: [],
-    filter: '',
-    showModal: false,
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    try {
+      const contactsInLS =
+        JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE)) || [];
+      setContacts(contactsInLS);
+
+      if (contactsInLS.length === 0) {
+        setTimeout(() => {
+          setShowModal(true);
+        }, 1000);
+      }
+    } catch (error) {
+      new Error(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    try {
+      localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(contacts));
+    } catch (error) {
+      new Error(error.message);
+    }
+  }, [contacts]);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem(
-        KEY_LOCAL_STORADGE,
-        JSON.stringify(this.state.contacts)
-      );
-    }
-  }
-
-  componentDidMount() {
-    const contactsInLS =
-      JSON.parse(localStorage.getItem(KEY_LOCAL_STORADGE)) || [];
-    this.setState({ contacts: contactsInLS });
-
-    if (contactsInLS.length === 0) {
-      setTimeout(() => {
-        this.setState({ showModal: true });
-      }, 1000);
-    }
-  }
-
-  toggleModal = () => {
-    this.setState(prevState => {
-      return {
-        showModal: !this.state.showModal,
-      };
-    });
-  };
-
-  handleInputFilter = evt => {
+  const handleInputFilter = evt => {
     const searchName = evt.target.value;
-    this.setState({ filter: searchName });
+    setFilter(searchName);
   };
 
-  handleSubmitForm = ({ id, name, number }) => {
-    const isExists = this.state.contacts.some(
+  const handleSubmitForm = ({ id, name, number }) => {
+    const isExists = contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
     if (isExists) {
       NotificationManager.info(`${name} is alredy in contacts`);
       return;
     }
-    this.setState(() => {
-      return {
-        contacts: [...this.state.contacts, { id, name, number }],
-      };
-    });
+    setContacts([...contacts, { id, name, number }]);
   };
 
-  handleDeleteContact = idContact => {
-    this.setState(prevState => {
-      return {
-        ...prevState,
-        contacts: prevState.contacts.filter(
-          contact => contact.id !== idContact
-        ),
-      };
-    });
+  const handleDeleteContact = idContact => {
+    setContacts(contacts.filter(contact => contact.id !== idContact));
   };
 
-  loadDefaultContacts = contactsFromModal => {
-    this.setState(prevState => {
-      return { ...prevState, contacts: contactsFromModal };
-    });
-    this.toggleModal();
+  const loadDefaultContacts = contactsFromModal => {
+    setContacts(contactsFromModal);
+    toggleModal();
   };
 
-  render() {
-    const { showModal } = this.state;
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-    const filteredContacts = this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
-    );
-
-    return (
-      <>
-        {showModal && (
-          <Modal
-            toggleModal={this.toggleModal}
-            loadContacts={this.loadDefaultContacts}
+  return (
+    <>
+      {showModal && (
+        <Modal toggleModal={toggleModal} loadContacts={loadDefaultContacts} />
+      )}
+      <div className="container">
+        <NotificationContainer />
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitForm={handleSubmitForm} />
+        <h2>Contacts</h2>
+        <Filter onInputFilter={handleInputFilter} filter={filter} />
+        {filteredContacts.length > 0 && (
+          <Contacts
+            contacts={filteredContacts}
+            onClickDelBtn={handleDeleteContact}
           />
         )}
-        <div className="container">
-          <NotificationContainer />
-          <h1>Phonebook</h1>
-          <ContactForm onSubmitForm={this.handleSubmitForm} />
-          <h2>Contacts</h2>
-          <Filter
-            onInputFilter={this.handleInputFilter}
-            filter={this.state.filter}
-          />
-          {filteredContacts.length > 0 && (
-            <Contacts
-              contacts={filteredContacts}
-              onClickDelBtn={this.handleDeleteContact}
-            />
-          )}
-        </div>
-      </>
-    );
-  }
-}
+      </div>
+    </>
+  );
+};
